3cc5be0db2fddadf22f87119370f7a5cda881b1e
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-13 11:43:42 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let selected = document.querySelector(`#colorSelect`)
+let button = document.querySelector(`.button`)
+       button.addEventListener(`click`, remove)
+
+function remove() {
+	   if(selected.selectedIndex !== -1){
+		   selected.remove(selected.selectedIndex)
+	   }
+	}
